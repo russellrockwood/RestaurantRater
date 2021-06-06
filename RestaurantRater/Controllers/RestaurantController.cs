ade1e65d7dc7b8b286d9036319f53e0d26cadbf8
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-06 04:18:13 -0400

    Add: Post/GetAll/GetById controller methods

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,54 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRestaurant([FromBody]Restaurant model)
+        {
+            if (model is null)
+                return BadRequest("Request body cannot be empty.");
+
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok("Restaurant Created");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+            
+        } 
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri]int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+                return Ok(restaurant);
+
+            return NotFound();
+        } 
+
+
+    }
+}
